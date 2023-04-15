108c349c19bb0c172280b3a89149ab233493df75
aliiahmadi <aliahmadi.github@gmail.com>
2023-04-15 18:07:22 +0330

    create models

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id: int = Column(Integer, primary_key=True, index=True)
+    email: str = Column(String(60), index=True)
+    password: str = Column(String(40))
+
+class Quote(Base):
+    __tablename__ = "quotes"
+    id: int = Column(Integer, primary_key=True, index=True)
+    text: str = Column(Text, index=True)
+    author: str = Column(String(30), index=True)
+
