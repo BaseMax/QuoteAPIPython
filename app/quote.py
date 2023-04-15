dfc40d1bcefb64c1dcb2a26890deb55514c159ae
aliiahmadi <aliahmadi.github@gmail.com>
2023-04-15 19:43:42 +0330

    handle random quote

--- app/quote.py
+++ app/quote.py
@@ -1,18 +1,22 @@
-from sqlalchemy.orm import Session
 from fastapi import Depends, HTTPException
+from fastapi.responses import JSONResponse
+import random
 
 from .models import Quote as QuoteModel
 from app.database import get_db
 
 class Quote:
-    """Quote class can use for get Quotes and create and ...
+    """
+        Quote class can use for get Quotes and create and ...
     """
     
     def __init__(self, db = Depends(get_db)) -> None:
         self.db = db
+        
     
     def get_all(self):
         return self.db.query(QuoteModel).all()
+    
     
     def get_by_id(self, quote_id: int):
         quote = self.db.query(QuoteModel).filter(QuoteModel.id == quote_id).first() 
@@ -20,3 +24,15 @@ class Quote:
             return quote
         return HTTPException(status_code=404, detail="quote not found.")
     
+    
+    def get_random(self):
+        count = self.get_count()
+        if count == 0:
+            return JSONResponse({
+                "detail": "can not found any quote.",
+            }, status_code=404)
+        return self.get_by_id(random.randint(1, count))
+    
+    
+    def get_count(self):
+        return self.db.query(QuoteModel).count()
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import Session
 
 from app.database import get_db
 from app.models import User
-# from app.schema import Quote
 from app.quote import Quote 
 
 
@@ -13,11 +12,19 @@ application = FastAPI()
 async def get_all_quotes(db: Session = Depends(get_db)):
     quote = Quote(db)
     return quote.get_all()
+
+
     
+@application.get("/quotes/random")
+async def get_random_quote(db: Session = Depends(get_db)):
+    quote = Quote(db)
+    return quote.get_random()
+
     
 
 @application.get("/quoets/{quote_id}")
 async def get_quote_by_id(quote_id: int,db: Session = Depends(get_db)):
     quote = Quote(db)
     return quote.get_by_id(quote_id)
+
 
