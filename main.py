554bc12eb896f9453c01fa36bab8c69baeca1752
Max Base <MaxBaseCode@Gmail.Com>
2023-04-16 08:33:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
 
-from app.database import get_db, get_token
 from app.quote import Quote
 from app.schema import Quote as Qu
+from app.database import get_db, get_token
 
 
 application = FastAPI()
@@ -14,13 +14,11 @@ async def get_all_quotes(db: Session = Depends(get_db)):
     return quote.get_all()
 
 
-    
 @application.get("/quotes/random")
 async def get_random_quote(db: Session = Depends(get_db)):
     quote = Quote(db)
     return quote.get_random()
 
-    
 
 @application.get("/quoets/{quote_id}")
 async def get_quote_by_id(quote_id: int,db: Session = Depends(get_db)):
@@ -28,14 +26,10 @@ async def get_quote_by_id(quote_id: int,db: Session = Depends(get_db)):
     return quote.get_by_id(quote_id)
 
 
-
 @application.post("/quotes")
 async def add_quote(UserQuote: Qu, db: Session = Depends(get_db), token: str = Depends(get_token)):
     quote = Quote(db, token)
     return quote.create(UserQuote)
-
-
-
 @application.put("/quotes/{quote_id}")
 async def edit_quote(quote_id: int, UserQuote: Qu, db: Session = Depends(get_db), token: str = Depends(get_token)):
     quote = Quote(db, token)
@@ -46,4 +40,3 @@ async def edit_quote(quote_id: int, UserQuote: Qu, db: Session = Depends(get_db)
 async def delete_quote(quote_id: int, db: Session = Depends(get_db), token: str = Depends(get_token)):
     quote = Quote(db, token)
     return quote.delete(quote_id)
-
